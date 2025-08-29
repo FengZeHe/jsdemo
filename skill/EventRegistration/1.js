d8d64dcc0ab2d081c196dbec8c469120d4ede550
hezefeng <hzf1998@126.com>
2025-08-29 14:57:04 +0800

    update

--- skill/EventRegistration/1.js
+++ skill/EventRegistration/1.js
@@ -1,11 +1,7 @@
-
 for (var i = 0; i < 3; i++) {
-    help(i)
-}
-
-function help(a) {
-    //每次循环产生一个新的作用域，作用域中的变量跟i相同
-    setTimeout(function () {
-        console.log(a)
-    }, 1000)
+    (function (a) {
+        setTimeout(function () {
+            console.log(a)
+        }, 1000)
+    })(i)
 }
